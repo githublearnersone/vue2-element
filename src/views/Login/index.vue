300a3005af2bfba2196ff723dbc5ab826a0f4523
井新鹏 <792967990@qq.com>
2020-11-04 14:28:07 +0800

    11.4(1)

---
+++ src/views/Login/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="login">
+        Login。。。
+        <el-button>默认按钮</el-button>
+    </div>
+</template>
+<script>
+export default {
+    name: 'login'
+}
+</script>
+<style lang="scss" scoped>
+#login {
+    height: 100vh;
+    background-color: #344a5f;
+}
+</style>
